3c60397b5ff5b0c520c93d79021902220acff326
wwang33 <wwang33@ncsu.edu>
2020-07-19 23:42:32 -0400

    task2 complete

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,9 @@
 public class Palindrome {
+    public Deque<Character> wordToDeque(String word){
+        Deque<Character> d = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            d.addLast(word.charAt(i));
+        }
+        return d;
+    }
 }
