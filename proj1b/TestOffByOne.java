8e7ba36466e87a8542e24c51c34bad8ed0335187
wwang33 <wwang33@ncsu.edu>
2020-07-22 12:12:37 -0400

    skeleton for task 4

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -1,10 +1,17 @@
-/*import org.junit.Test;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    // You must use this CharacterComparator and not instantiate
-    // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
 
+    public static void main(String[] args) {
+        // You must use this CharacterComparator and not instantiate
+        // new ones, or the autograder might be upset.
+        Boolean r = offByOne.equalChars('a', 'a');
+        System.out.print("r: " + r);
+    }
+
+    //    ("r: "+ r);
+
     // Your tests go here.
-} Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
+}
